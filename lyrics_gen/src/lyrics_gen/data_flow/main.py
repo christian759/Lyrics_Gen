35b5b34f59da10a2e949020bca1c2f8711388952
CEO1 <christian4onos@outlook.com>
2024-09-21 22:51:12 +0100

    .xlxs

--- lyrics_gen/src/lyrics_gen/data_flow/main.py
+++ lyrics_gen/src/lyrics_gen/data_flow/main.py
@@ -1,5 +1,5 @@
 from lyrics_extractor import SongLyrics
 import pandas as pd
 
-data = pd.read_csv("data.csv")
-print(data)
+data = pd.read_csv("C:\\Users\\chris\\python\\lab\\Lyrics_Gen\\lyrics_gen\\src\\lyrics_gen\\data_flow\\data.csv")
+print(data['Name'])
