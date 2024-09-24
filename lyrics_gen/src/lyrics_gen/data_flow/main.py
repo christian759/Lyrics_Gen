b46f10d7fb5f86811d067c01446763be289a3a5a
CEO1 <christian4onos@outlook.com>
2024-09-24 21:22:46 +0100

    main

--- lyrics_gen/src/lyrics_gen/data_flow/main.py
+++ lyrics_gen/src/lyrics_gen/data_flow/main.py
@@ -1,5 +1,9 @@
-from lyrics_extractor import SongLyrics
 import pandas as pd
 
-data = pd.read_csv("C:\\Users\\chris\\python\\lab\\Lyrics_Gen\\lyrics_gen\\src\\lyrics_gen\\data_flow\\data.csv")
-print(data['Name'])
+data = pd.read_csv("C:/Users/chris/python/lab/Lyrics_Gen/lyrics_gen/src/lyrics_gen/resources/song.csv")
+song = data['song']
+text = data['text']
+
+num_of_words =
+
+    
